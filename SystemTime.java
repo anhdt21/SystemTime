da9877102a7a513cad77acd499d98b444d58a2eb
SON NGUYEN <myantj3m@gmail.com>
2018-07-31 10:20:08 +0700

    Show System time

---
+++ SystemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date Present = new Date();
+        System.out.printf("Now is :" + Present);
+    }
+}
